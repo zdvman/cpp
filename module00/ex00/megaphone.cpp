9f7b54f11abe922531a8e4b216bca75a3b194133
Dmytro Zuiev <dzuiev@c2r3s1.42london.com>
2024-07-03 17:00:08 +0100

    Initial commit

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dzuiev <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/03 11:31:54 by dzuiev            #+#    #+#             */
+/*   Updated: 2024/07/03 12:20:17 by dzuiev           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	}
+	else
+	{
+		for (int i = 1; i < argc; ++i)
+		{
+			for (int j = 0; argv[i][j] != '\0'; ++j)
+			{
+				std::cout << static_cast<char>(std::toupper(argv[i][j]));
+			}
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
